59de9d4e3cf352e6bac0b3e5f5eccc765c3d0a98
victorar <victorar@microsoft.com>
2017-04-05 17:41:56 +0200

    Add script to configure NVA
    Adding script that configures Windows Server 2016 NVA to NAT traffic

---
+++ Internet-breakout-via-NVA/Configure-NVA.ps1
@@ -0,0 +1,18 @@
+﻿#Install Routing Feature
+Install-WindowsFeature -Name "Routing" -IncludeAllSubFeature -IncludeManagementTools
+
+#Rename network adapters (External has outbout connectivity, and internal does not)
+Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | Rename-NetAdapter -NewName "External" -PassThru
+Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -eq $null} | Rename-NetAdapter -NewName "Internal" -PassThru
+
+#Get the IP Address of the External Adapter
+$extIPAddress = Get-NetIPAddress -InterfaceAlias External | Where-Object {$_.AddressFamily -eq "IPV4"}
+
+#Get the NAT IP Address as well as the NAT external address space
+$natIP = $extIPAddress.IPAddress
+$netPrefix = $extIPAddress.PrefixLength
+$extAddressPrefix = $natIP.Substring(0,$natIP.LastIndexOf(".")) + ".0/" + $netPrefix
+
+#Configure NAT
+New-NetNat -Name 'NVANAT' -ExternalIPInterfaceAddressPrefix $extAddressPrefix
+Add-NetNatExternalAddress -NatName 'NVANAT' -IPAddress $natIP -PortStart 5000 -PortEnd 49151
