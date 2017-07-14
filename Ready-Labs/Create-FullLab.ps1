ff42067733f8be0eb1c5fb4197d8c8b8898760f5
victorar <victorar@microsoft.com>
2017-07-15 00:55:27 +0200

    Adding script to deploy end-to-end lab
    Script that deploys the end-to-end lab: hub & spoke, ExpressRoute
    gateway and VNet peering

---
+++ Ready-Labs/Create-FullLab.ps1
@@ -0,0 +1,13 @@
+
+$rgName = "ReadyLab-Networking"
+$location = "West US"
+$templateUri = "https://raw.githubusercontent.com/victorar/azure-hybrid-networking/master/Ready-Labs/FullLab/azuredeploy.json"
+
+Login-AzureRmAccount
+
+New-AzureRmResourceGroup -Name $rgName -Location $location
+New-AzureRmResourceGroupDeployment `
+        -Name "DeployNetworkResources" `
+        -ResourceGroupName $rgName `
+        -TemplateUri $templateUri `
+        -Verbose
