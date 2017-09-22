4f063827fcef488f67836cf70f965175f739e0ab
victorar <victorar@microsoft.com>
2017-09-22 14:10:03 +0200

    Uploading scripts
    Uploading Ignite scripts

---
+++ Ignite-Labs/Create-SpokeVNet.ps1
@@ -0,0 +1,13 @@
+
+$rgName = "IgniteUserRG"
+$location = "West US"
+$templateUri = "https://raw.githubusercontent.com/victorar/azure-hybrid-networking/master/Ready-Labs/HubAndSpoke/azuredeploy.json"
+
+Login-AzureRmAccount
+
+New-AzureRmResourceGroup -Name $rgName -Location $location
+New-AzureRmResourceGroupDeployment `
+        -Name "DeployNetworkResources" `
+        -ResourceGroupName $rgName `
+        -TemplateUri $templateUri `
+        -Verbose
