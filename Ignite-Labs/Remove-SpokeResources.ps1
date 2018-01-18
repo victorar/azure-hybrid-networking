16b9bacf4038dd7262d12f48b4dfb440f5cee406
victorar <victorar@microsoft.com>
2018-01-18 15:50:56 +0100

    Uploading Ready scripts
    Uploading Ready scripts

---
+++ Ignite-Labs/Remove-SpokeResources.ps1
@@ -0,0 +1,15 @@
+﻿
+#Login-AzureRmAccount
+
+$rgName = "IgniteUserRG"
+$spokeAddressSpace = "10.100.0.0/16"
+
+$vNets = Get-AzureRmVirtualNetwork -ResourceGroupName $rgName
+
+foreach($vnet in $vNets){
+    if($vnet.AddressSpace.AddressPrefixes -eq $spokeAddressSpace){
+        Write-Host "Deleting VNet..." $vnet.Name -ForegroundColor Cyan
+        $vnet | Remove-AzureRmVirtualNetwork -Force
+    }
+}
+
