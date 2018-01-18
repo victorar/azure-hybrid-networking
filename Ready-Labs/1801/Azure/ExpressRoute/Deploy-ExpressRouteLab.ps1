a6dd84633d774d8801f887e475f5a2204dc26239
victorar <victorar@microsoft.com>
2018-01-18 17:42:39 +0100

    Adding sample script to deploy template
    Adding sample script to deploy template

---
+++ Ready-Labs/1801/Azure/ExpressRoute/Deploy-ExpressRouteLab.ps1
@@ -0,0 +1,14 @@
+
+$rgName = "Azure-ReadyLab"
+$location = "West US"
+$templateUri = "https://raw.githubusercontent.com/victorar/azure-hybrid-networking/master/Ready-Labs/1801/Azure/ExpressRoute/azuredeploy.json"
+
+Login-AzureRmAccount
+
+Remove-AzureRmResourceGroup -Name $rgName -Force -ErrorAction Ignore
+New-AzureRmResourceGroup -Name $rgName -Location $location
+New-AzureRmResourceGroupDeployment `
+        -Name "ReadyLab-AzureDeployment" `
+        -ResourceGroupName $rgName `
+        -TemplateUri $templateUri `
+        -Verbose
